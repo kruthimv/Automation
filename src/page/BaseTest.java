2fe9ef1e305bd8df3728bddd8c6976615d048412
kruthimv <43552065+kruthimv@users.noreply.github.com>
2018-09-26 00:03:32 +0530

    deleting my class

---
+++ src/page/BaseTest.java
@@ -0,0 +1,31 @@
+package page;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest {
+	static
+	{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+	public WebDriver driver;
+	@BeforeMethod
+	public void preCondition()
+	{
+ 		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://demo.actitime.com/login.do");
+		
+	}
+	@AfterMethod
+	public void postCondition()
+	{
+		driver.close();
+	}
+
+}
---
+++ src/page/Test.java
@@ -0,0 +1,10 @@
+package page;
+public class Test extends BaseTest{
+	@org.testng.annotations.Test
+	public void testA()
+	{
+		
+		
+	}
+
+}
