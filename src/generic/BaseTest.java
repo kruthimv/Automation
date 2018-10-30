8509e01c827dce48d1a34a38e91d79bc77bb7a3b
kruthimv <43552065+kruthimv@users.noreply.github.com>
2018-10-30 17:43:58 +0530

    test.java commited from orangeapp

--- src/generic/BaseTest.java
+++ src/generic/BaseTest.java
@@ -4,28 +4,61 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
-public abstract class BaseTest {
-	static
-	{
-		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-	}
+@Listeners(Result.class)
+public abstract class BaseTest implements IAutoConst {
 	public WebDriver driver;
-	@BeforeMethod
-	public void preCondition()
-	{
- 		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("https://demo.actitime.com/login.do");
-		
+	public String url = Utility.getPropertyValue(CONFIG_PATH, "URL");
+
+	String strITO = Utility.getPropertyValue(CONFIG_PATH, "ITO");
+	public long lngITO = Long.parseLong(strITO);
+
+	public String strETO = Utility.getPropertyValue(CONFIG_PATH, "ETO");
+	public long lngETO = Long.parseLong(strETO);
+
+	static {
+		System.setProperty(CHROME_KEY, CHROME_VALUE);
+		System.setProperty(GECKO_KEY, GECKO_VALUE);
 	}
+	//
+
+	@Parameters({ "ip", "browser" })
+	@BeforeMethod
+	public void openApp(@Optional("localhost") String ip, @Optional("chrome") String browser) {
+		driver = Utility.openBrowser(driver, "localhost", "chrome");
+		// driver = new ChromeDriver();// here remove webdriver , because to avoid null
+		// pointer exception
+		// in the line 29 we have to remove webdriver in the line 19, we have to
+		// initialize the driver
+		// varriable globally
+		driver.manage().window().maximize();
+		// System.out.println(duruation);
+		driver.manage().timeouts().implicitlyWait(lngITO, TimeUnit.SECONDS);
+		driver.get(url);
+
+	}
+
 	@AfterMethod
-	public void postCondition()
-	{
-		driver.close();
+
+	public void closeApp(ITestResult result) {
+		String name = result.getName();
+		int status = result.getStatus();
+		if (status == 2) {
+			String path = Utility.getPhoto(driver, PHOTO_PATH);
+			Reporter.log("Test:" + name + "is Failed and Photo is:" + path, true);
+
+		} else {
+			Reporter.log("Test:" + name + "is Passed", true);
+		}
+		driver.quit();
 	}
 
 }
